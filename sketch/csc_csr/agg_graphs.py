8363c9acb8366a4e92f2a64f3fb50f471bba339f
niklas <niklas@nik.run>
2024-05-13 15:48:41 +0000

    checkin results

--- sketch/csc_csr/agg_graphs.py
+++ sketch/csc_csr/agg_graphs.py
@@ -1,6 +1,8 @@
 import os
 import json
 import glob
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 result_dir =  'results'
@@ -15,16 +17,12 @@ for n in ns:
         exp=result_dir+"/"+n+"/"+s+"-"+n
         files_per_param[exp] = {}
         for f in folders:
-            print(exp)
-            print(f)
             files_per_param[exp][f] = glob.glob(os.path.join(exp+"/"+f, '**/*.json'), recursive=True)
 
 values = {}
 for key, value in files_per_param.items():
-    print(key)
     values[key] = {}
     for key2, value2 in value.items():
-        print(key2)
         aggregate = {
             "cycles": [],
             "real_time_nsec": [],
@@ -42,7 +40,9 @@ for key, value in files_per_param.items():
         values[key][key2] = aggregate
 
 moi = ["real_time_nsec", "perf::CACHE-MISSES"]
+unit = ["ns", "# num"]
 for i, (key, value) in enumerate(values.items()):
+    print(key)
     fig, axes = plt.subplots(nrows=len(moi), ncols=1, figsize=(8, 12))
     _moi = {}
     for m in moi:
@@ -51,15 +51,18 @@ for i, (key, value) in enumerate(values.items()):
             _moi[m][key2] = value2[m]    
 
     for j, (key3, value3) in enumerate(_moi.items()):
-        print(j, key3, value3)
-        agg_names = key3
+
+        agg_names = value3.keys()
         metrics = list(value3.values())
         metrics = [sorted(metrics[i]) for i in range(0, len(metrics))]
-        print(metrics)
-        ax = axes[j]
-        ax.boxplot(metrics, agg_names)
-        ax.set_title(f'{key3}')
-        ax.set_ylabel('Value')
+ 
+        df = pd.DataFrame(np.array(metrics).T, columns=agg_names)
+        ax1 = axes[j]
+        df.boxplot(column=list(agg_names), grid=True, ax=ax1)
+        #ax1.set_ylim(ymin=0)
+        ax1.set_title(f'{key3}')
+        ax1.set_xlabel(f'x data')
+        ax1.set_ylabel(f'{unit[j]}')
 
     plt.tight_layout()
     plt.savefig(f'{key}/boxplot.png', dpi=300)
