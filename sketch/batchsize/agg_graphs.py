bd3f0980c7078cdf7a0ee72d1210b415f782cd2b
niklas <niklas@nik.run>
2024-05-16 19:22:23 +0000

    checkin

--- sketch/batchsize/agg_graphs.py
+++ sketch/batchsize/agg_graphs.py
@@ -5,42 +5,101 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-result_dir =  'results'
-sub = ["long", "sym", "wide"]
-folders = ["csrcsr-aggCol","csccsc-aggCol","csrcsr-aggRow","csccsc-aggRow"]
+result_dir =  'results/100000_100'
+HEIGHT=100000
+WIDTH=100
 
 ns = [name for name in os.listdir(result_dir) if os.path.isdir(os.path.join(result_dir, name))]
 
 files_per_param = {}
 for n in ns:
-    for s in sub:
-        exp=result_dir+"/"+n+"/"+s+"-"+n
-        files_per_param[exp] = {}
-        for f in folders:
-            files_per_param[exp][f] = glob.glob(os.path.join(exp+"/"+f, '**/*.json'), recursive=True)
+    files_per_param[n] = glob.glob(os.path.join(result_dir+"/"+n, '**/*.json'), recursive=True)
 
 values = {}
 for key, value in files_per_param.items():
     values[key] = {}
-    for key2, value2 in value.items():
-        aggregate = {
-            "cycles": [],
-            "real_time_nsec": [],
-            "perf::CYCLES": [],
-            "perf::INSTRUCTIONS": [],
-            "perf::CACHE-REFERENCES": [],
-            "perf::CACHE-MISSES": [],
-            "perf::BRANCHES": []
-        }
-        for file in value2:
-            with open(file, 'r') as f:
-                data = json.load(f)
-                for m in aggregate:
-                    aggregate[m].append(int(data['threads']['0']['regions']['0'][m]))
-        values[key][key2] = aggregate
+    aggregate = {
+        "cycles": [],
+        "real_time_nsec": [],
+        "perf::CYCLES": [],
+        "perf::INSTRUCTIONS": [],
+        "perf::CACHE-REFERENCES": [],
+        "perf::CACHE-MISSES": [],
+        "perf::BRANCHES": []
+    }
+    for value2 in value:
+        with open(value2, 'r') as f:
+            data = json.load(f)
+            for m in aggregate:
+                aggregate[m].append(int(data['threads']['0']['regions']['0'][m]))
+    values[key] = aggregate
+
+_keys = [int(k) for k in list(values.keys())]
+_keys.sort()
+values = {i: values[str(i)] for i in _keys}
 
 moi = ["real_time_nsec", "perf::CACHE-MISSES"]
 unit = ["ns", "# num"]
+
+filter_key = 50
+
+_moi = {}
+for m in moi:
+    _moi[m] = {}
+    for j, (key2, value2) in enumerate(values.items()):
+        if filter_key == -1 or filter_key > key2:
+            _moi[m][key2] = value2[m]
+
+print
+
+_std_moi = {}
+for key, values in _moi.items():
+    _std_moi[key] = {}
+    for key2, value2 in enumerate(values.items()):
+        _std_moi[key][value2[0]] = np.std(value2[1])
+
+mean_moi = {}
+for key, value in _moi.items():
+    mean_moi[key] = {k: round(sum(v)/len(v)) for k, v in value.items()}
+
+'''
+print("MAX")
+print(max_moi[list(max_moi.keys())[0]])
+print("MEAN")
+print(mean_moi[list(mean_moi.keys())[0]])
+print("MIN")
+print(min_moi[list(min_moi.keys())[0]])
+'''
+
+
+
+fig, axes = plt.subplots(nrows=len(moi), ncols=1, figsize=(8, 12))
+for j, key3 in enumerate(_moi.keys()):
+
+        ax1 = axes[j]
+        mean_lists = sorted(mean_moi[key3].items())
+        std_lists = sorted(_std_moi[key3].items())
+        x, y = zip(*mean_lists)
+        _, e = zip(*std_lists)
+
+        lower_bound = tuple(map(lambda i, j: i - j, y, e))
+        upper_bound = tuple(map(lambda i, j: i + j, y, e))
+
+        #ax1.plot(x, y, color="grey")
+        ax1.plot(x, y, color="red")
+        ax1.fill_between(x, lower_bound, upper_bound)
+        #ax1.plot(x2, y3, color="grey")
+        
+        #ax1.set_ylim(ymin=0)
+        ax1.set_title(f'{key3}')
+        ax1.set_xlabel(f'x data')
+        ax1.set_ylabel(f'{unit[j]}')
+
+plt.tight_layout()
+plt.savefig(f'{result_dir}/result.png', dpi=300)
+
+
+'''
 for i, (key, value) in enumerate(values.items()):
     print(key)
     fig, axes = plt.subplots(nrows=len(moi), ncols=1, figsize=(8, 12))
@@ -66,3 +125,4 @@ for i, (key, value) in enumerate(values.items()):
 
     plt.tight_layout()
     plt.savefig(f'{key}/boxplot.png', dpi=300)
+'''
--- sketch/batchsize/run_exp.py
+++ sketch/batchsize/run_exp.py
@@ -9,13 +9,14 @@ PAPI_EVENTS = "perf::CYCLES,perf::INSTRUCTIONS,perf::CACHE-REFERENCES,perf::CACH
 PAPI_REPORT = 1
 
 NUM_RUNS=3
-STEPS=10
+STEPS=100
 
 WIDTH=100
-HEIGHT=1000000
+HEIGHT=100000
 
 #https://stackoverflow.com/questions/12418234/logarithmically-spaced-integers
 def gen_log_space(limit, n):
+    n = n+1
     result = [1]
     if n>1:  # just a check to avoid ZeroDivisionError
         ratio = (float(limit)/result[-1]) ** (1.0/(n-len(result)))
@@ -30,7 +31,7 @@ def gen_log_space(limit, n):
             # recalculate the ratio so that the remaining values will scale correctly
             ratio = (float(limit)/result[-1]) ** (1.0/(n-len(result)))
     # round, re-adjust to 0 indexing (i.e. minus 1) and return np.uint64 array
-    return np.array(list(map(lambda x: round(x)-1, result)), dtype=np.uint64)
+    return np.array(list(map(lambda x: round(x)-1, result)), dtype=np.uint64)[1:]
 
 name = str(HEIGHT) + "_" + str(WIDTH)
 os.makedirs(name, exist_ok=True)
@@ -39,10 +40,14 @@ os.chdir(name)
 logspace = gen_log_space(HEIGHT, STEPS)
 
 for batchSize in logspace:
-    
+
     args = [DAPHNE_PATH, "--num-threads=1", "--vec", f"--batchSize={batchSize}", "--args", f"n={HEIGHT},m={WIDTH}", f"../../home-niklas/daphne/sketch/batchsize/n_m_matrix_unary_sqrt.daph"]
 
-    for _ in range(NUM_RUNS): 
+    for _ in range(NUM_RUNS):
+
+        os.makedirs(str(batchSize), exist_ok=True)
+        os.chdir(str(batchSize))
+
         print("\033[91m" + f"Running {HEIGHT} {WIDTH} {batchSize}" + "\033[0m")
 
         os.environ["PAPI_EVENTS"] = PAPI_EVENTS
