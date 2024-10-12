eaff8ee6737c595cbb7b7a944c51c111fbaf03d6
niklas <niklas@nik.run>
2024-10-12 17:28:54 +0000

    improv run_horz.py

--- sketch/run_horz.py
+++ sketch/run_horz.py
@@ -4,97 +4,16 @@ import numpy as np
 import subprocess
 import json
 import datetime
-
-to_print = False
-if "--print" in sys.argv:
-    to_print = True
-
-max_horz_ops=250
-min_horz_ops=1
-
-_env = {
-    "PAPI_EVENTS": "perf::CYCLES,perf::INSTRUCTIONS,perf::CACHE-REFERENCES,perf::CACHE MISSES,perf::BRANCHES,perf::BRANCH-MISSES",
-    #"PAPI_EVENTS": "perf::L1-dcache-load-misses,perf::L1-dcache-loads,perf::L1-dcache-prefetches,perf::L1-icache-load-misses,perf::L1-icache-loads",
-    "PAPI_REPORT": "1"
-}
-
-operators = [
-    "minus(X);",
-    "abs(X);",
-    "sign(X);",
-    "exp(X);",
-    "ln(X);",
-    "mod(X, 2);",
-    "sqrt(X);",
-    "round(X);",
-    "floor(X);",
-    "ceil(X);",
-    "sin(X);",
-    "cos(X);",
-    "tan(X);",
-    "sinh(X);",
-    "cosh(X);",
-    "tanh(X);",
-    "asin(X);",
-    "acos(X);",
-    "atan(X);",
-    "isNan(X);",
-    "pow(X, 2);",
-    "log(X, 2);",
-    "min(X, 2);",
-    "max(X, 2);",
-    "X + 0.5;",
-    "X - 0.5;",
-    "X * 0.5;",
-    "X / 0.5;",
-]
-
-def generate_operator(i, arg):
-    return "v" + str(i) + " = " + arg + " + " +  str(i*0.1) + ";"
-    
-num_points = 20
-log = np.logspace(np.log10(min_horz_ops), np.log10(max_horz_ops), num=num_points)
-
-#depth = int(sys.argv[1])
-#width = int(sys.argv[2])
-
-
-prefix = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-
-def generate_script(num_ops):
-
-    script = []
-    
-    script.append("X = fill(1.0, 10000, 10000);")
-    #script.append("start = now();")
-    script.append("startProfiling();")
-
-    count = 0
-
-    for j in range(0, num_ops):
-        #script.append("v"+str(count)+" = "+ operators[j])
-        script.append(generate_operator(j, "X"))
-        script.append("s"+str(j)+" = sum(v"+str(j)+");")
-        count = count + 1
-    script.append("stopProfiling();")
-    #script.append("end = now();")
-
-    for j in range(0, count):
-        #script.append("print(v"+str(j)+"[0,0]);")
-        script.append("print(s"+str(j)+");")
-
-    #script.append("print(\"F1XM3:\"+ (end - start));")
-
-    #script.insert(0, "#total: "+str(width*depth+num_input+width))
-    return script
-    
+import argparse
+from tabulate import tabulate
+import pandas as pd
 
 def extract_f1xm3(stdout):
     lines = stdout.split('\n')
 
     for line in reversed(lines):
-        if 'F1XM3' in line:
-            number = line.split('F1XM3:')[1]
+        if "F1XM3" in line:
+            number = line.split("F1XM3:")[1]
             return int(number)
     return None
 
@@ -108,94 +27,175 @@ def extract_papi(stdout):
            break
     t = "".join(lines[offset+1:])
     j = json.loads(t)
-    return j['threads']["0"]["regions"]["0"]
+    out = j["threads"]["0"]["regions"]["0"]
+    del out["name"]
+    del out["parent_region_id"]
+    return out
 
+#------------------------------------------------------------------------------
+# GLOBAL
+#------------------------------------------------------------------------------
 
-#command = ["../bin/daphne", "--vec-type=GREEDY_1", "--num-threads=1", "_horz.daph"]
-#command = ["../bin/daphne", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "_horz.daph"]
+MAX_HORZ_OPS = 11
+MIN_HORZ_OPS = 10
 
-cwd = "daphne-X86-64-vec-bin"
-commands = [["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize="+str(int(l)), "../_horz.daph"] for l in log]
-'''
-commands = [
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=1", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=5", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=25", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=50", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=100", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=250", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=1", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=5", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=25", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=50", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=100", "../_horz.daph"],
-    ["./run-daphne.sh", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "--batchSize=250", "../_horz.daph"],
+TOOLS = {
+    "PAPI_STD": {
+        "ENV": {
+            "PAPI_EVENTS": "perf::CYCLES,perf::INSTRUCTIONS,perf::CACHE-REFERENCES,perf::CACHE MISSES,perf::BRANCHES,perf::BRANCH-MISSES",
+            "PAPI_REPORT": "1"
+        },
+        "START_OP": "startProfiling();",
+        "STOP_OP": "stopProfiling();",
+        "END_OP": "",
+        "GET_INFO": extract_papi
+    },
+    "PAPI_L1": {
+        "ENV": {
+            "PAPI_EVENTS": "perf::L1-dcache-load-misses,perf::L1-dcache-loads,perf::L1-dcache-prefetches,perf::L1-icache-load-misses,perf::L1-icache-loads",
+            "PAPI_REPORT": "1",
+        },
+        "START_OP": "startProfiling();",
+        "STOP_OP": "stopProfiling();",
+        "END_OP": "",
+        "GET_INFO": extract_papi
+    },
+    "PAPI_MPLX": {
+        "ENV": {
+            "PAPI_EVENTS": "perf::CYCLES,perf::INSTRUCTIONS,perf::CACHE-REFERENCES,perf::CACHE MISSES,perf::BRANCHES,perf::BRANCH-MISSES,perf::L1-dcache-load-misses,perf::L1-dcache-loads,perf::L1-dcache-prefetches,perf::L1-icache-load-misses,perf::L1-icache-loads",
+            "PAPI_REPORT": "1",
+            "PAPI_MULTIPLEX": "1",
+        },
+        "START_OP": "startProfiling();",
+        "STOP_OP": "stopProfiling();",
+        "END_OP": "",
+        "GET_INFO": extract_papi
+    },
+    "NOW": {
+        "ENV": {},
+        "START_OP": "start = now();",
+        "STOP_OP": "end = now();",
+        "END_OP": "print(\"F1XM3:\"+ (end - start));",
+        "GET_INFO": extract_f1xm3
+    }
+}
+
+GENERATE_FUNCS = {
+    "ADD": lambda i, arg: [f"v{i} = {arg} + {i * 0.1};"],
+    "ADD_SUM": lambda i, arg: [f"i{i} = {arg} + {i * 0.1};", f"v{i} = sum(i{i});"]
+}
+
+GENERATE_PRINT_FUNCS = {
+    "ADD": lambda i: [f"print(v{i}[0,0]);"],
+    "ADD_SUM": lambda i: [f"print(v{i});"]
+}
+
+BASE_CWD = "daphne-X86-64-vec-bin"
+BASE_COMMAND = lambda th, bs, no_hf: [
+    "./run-daphne.sh",
+    "--timing",
+    "--vec",
+    "--vec-type=GREEDY_1",
+    f"--num-threads={th}",
+    f"--batchSize={bs}",
+    "--no-hf" if no_hf else "",
+    "../_horz.daph"
 ]
-'''
 
-samples = 3
+#------------------------------------------------------------------------------
+# HELPER
+#------------------------------------------------------------------------------
 
-'''
-cwd = "./"
-commands = [
-    ["../bin/daphne", "--timing", "--vec", "--vec-type=GREEDY_1", "--num-threads=1", "./_horz.daph"],
-    ["../bin/daphne", "--timing", "--vec", "--no-hf", "--vec-type=GREEDY_1", "--num-threads=1", "./_horz.daph"]
-]
-'''
+def generate_script(num_ops, tool, func, rows, cols):
 
-def run_command(command, cwd):
+    script = []
+    
+    script.append(f"X = fill(1.0, {rows}, {cols});")
+    script.append(TOOLS[tool]["START_OP"])
+
+    for j in range(0, num_ops):
+        script += GENERATE_FUNCS[func](j, "X")
+    script.append(TOOLS[tool]["STOP_OP"])
+
+    for j in range(0, num_ops):
+        script += GENERATE_PRINT_FUNCS[func](j)
+
+    script.append(TOOLS[tool]["END_OP"])
+
+    return script
+
+
+def run_command(command, cwd, env):
     _command = []
     _command += command 
 
-    process = subprocess.Popen(_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=_env)
+    process = subprocess.Popen(_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env={**env, **os.environ})
     stdout, stderr = process.communicate()
 
     return stdout.decode(), stderr.decode()
 
-#for ops in log:
+#------------------------------------------------------------------------------
+# ARGS
+#------------------------------------------------------------------------------
 
-output = []
-for c in commands: 
+parser = argparse.ArgumentParser(description="Arguments")
+parser.add_argument("--tool", type=str, choices=["PAPI_STD", "PAPI_L1", "PAPI_MPLX", "NOW"], help="", required=True)
+parser.add_argument("--script", type=str, choices=["ADD", "ADD_SUM"], help="", required=True)
+parser.add_argument("--rows", type=int, default=10000, help="rows")
+parser.add_argument("--cols", type=int, default=10000, help="rows")
+parser.add_argument("--samples", type=int, default=3, help="")
+parser.add_argument("--num-ops", type=int, default=12, help="")
 
-    
-    env_str = ""
-    for k, v in _env.items():
-        env_str += k +"=\""+v+"\" "
+#------------------------------------------------------------------------------
+# MAIN
+#------------------------------------------------------------------------------
 
-    _out = {}
-    for ops in range(16, 17):
-    #for ops in range(0, 14):
+if __name__ == "__main__":
 
-        print("Run: " + env_str + " ".join(c) + " " + str(int((ops))))
+    args = parser.parse_args()
+    exp_start = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
-        script = generate_script(int(ops))
-        with open("_horz.daph", "w") as f:
-            for line in script:
-                f.write(line + '\n')
-
-        timings = []
-        for i in range(0, samples):
-            stdout, stderr = run_command(c, cwd)
-            
-            timing = json.loads(stderr)
-            #timing["vectorized_nanoseconds"] = extract_f1xm3(stdout)
-            timing["papi"] = extract_papi(stdout)
-
-            print(timing)
-            timings.append(timing)
-       
-        _out[ops] = timings 
-
-        print()
+    output = []
+    for no_hf in [False, True]: 
         
-    output.append({
-        "cmd": c,
-        "timings": _out
-    })
+        tool_env = TOOLS[args.tool]["ENV"]
+        command = BASE_COMMAND(1, 0, no_hf)
 
+        env_str = " ".join(f"{k}=\"{v}\"" for k, v in tool_env.items())
+        command_str = " ".join(command)
 
-with open(prefix + "-horz_timings.json", "w+") as f:
-    json.dump(output, f, indent=4)
-    f.close()
+        command_output = {}
+        for ops in range(args.num_ops, args.num_ops+1):
+
+            print(f"Run: {env_str} {command_str} {ops}")
+
+            script = generate_script(ops, args.tool, args.script, args.rows, args.cols)
+            with open("_horz.daph", "w") as f:
+                for line in script:
+                    f.write(line + '\n')
+
+            timings = []
+            for i in range(0, args.samples):
+                stdout, stderr = run_command(command, BASE_CWD, tool_env)
+                
+                timing = json.loads(stderr)
+                timing["tool"] = TOOLS[args.tool]["GET_INFO"](stdout)
+
+                df = pd.json_normalize(timing, sep=".")
+                print(tabulate(df, headers="keys", tablefmt="psql", showindex=False))
+                timings.append(timing)
+        
+            command_output[ops] = timings 
+
+            print()
+            
+        output.append({
+            "cmd": command,
+            "timings": command_output,
+            "min-ops": MIN_HORZ_OPS,
+            "max-ops": MAX_HORZ_OPS
+        })
+
+    with open(exp_start + "-horz_timings.json", "w+") as f:
+        json.dump(output, f, indent=4)
+        f.close()
